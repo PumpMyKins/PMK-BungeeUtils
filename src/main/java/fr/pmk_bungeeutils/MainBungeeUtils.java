c7772b66dd0653110535bc5d55425ea4ee319c1c
Clem-Fern-Dev <Clement@Clem-PC>
2018-04-28 20:57:06 +0200

    Ajout de la sauvegarde de la config au OnDisable

--- src/main/java/fr/pmk_bungeeutils/MainBungeeUtils.java
+++ src/main/java/fr/pmk_bungeeutils/MainBungeeUtils.java
@@ -1,5 +1,48 @@
 package fr.pmk_bungeeutils;
 
-public class MainBungeeUtils {
+import fr.pmk_bungeeutils.blockmod.BlockModCommand;
+import fr.pmk_bungeeutils.blockmod.BlockModListener;
+import fr.pmk_bungeeutils.blockmod.BlockModManager;
+import fr.pmk_bungeeutils.config.ConfigUtils;
+import net.md_5.bungee.api.plugin.Plugin;
 
+public class MainBungeeUtils extends Plugin{
+
+	private static ConfigUtils configUtils;
+	
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+		configUtils = ConfigUtils.getConfig(this);
+		
+		configUtils.initDataFolder();
+		configUtils.initAndGetFile("config.yml");		
+		
+		BlockModManager.init(configUtils);
+		
+		//initiasliation
+		getProxy().getPluginManager().registerListener(this, new BlockModListener());
+		getProxy().getPluginManager().registerCommand(this, new BlockModCommand());
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		BlockModManager.save(configUtils);
+	}
+	
+	@Override
+	public void onLoad() {
+		// TODO Auto-generated method stub
+	}
+
+	public static ConfigUtils getConfigUtils() {
+		return configUtils;
+	}
+
+	public static void setConfigUtils(ConfigUtils configUtils) {
+		MainBungeeUtils.configUtils = configUtils;
+	}
+	
 }
