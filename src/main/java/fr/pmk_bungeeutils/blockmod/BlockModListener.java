03915fc02578878f1dbb3e25c7ffa9d069f96eed
Clem-Fern-Dev <Clement@Clem-PC>
2018-04-28 20:56:21 +0200

    Fix debug message

--- src/main/java/fr/pmk_bungeeutils/blockmod/BlockModListener.java
+++ src/main/java/fr/pmk_bungeeutils/blockmod/BlockModListener.java
@@ -1,9 +1,25 @@
 package fr.pmk_bungeeutils.blockmod;
 
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
 
 public class BlockModListener implements Listener {
 
-	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void OnPlayerLog(PostLoginEvent e) {
+		
+		ProxiedPlayer p = e.getPlayer();
+		
+		if(BlockModManager.isActive()) {
+			if(!p.hasPermission(BlockModManager.getOverPerm())) {
+
+				p.disconnect("[PUMPMYCORD] Vous ne pouvez pas vous connecter au serveur ! (" + BlockModManager.getRaison() + ")");
+			}
+		}
+		
+	}
 	
 }
