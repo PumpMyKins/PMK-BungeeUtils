573700bd67486ca6877aeb884c698ca747a82b3f
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-04-28 22:39:49 +0200

    Fix des listeners, ajout/del des hashMaps

--- src/main/java/fr/pmk_bungeeutils/support/SupportListener.java
+++ src/main/java/fr/pmk_bungeeutils/support/SupportListener.java
@@ -1,7 +1,34 @@
 package fr.pmk_bungeeutils.support;
 
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
 
 public class SupportListener implements Listener {
 
+	@EventHandler
+	public void OnPlayerJoin(PostLoginEvent e) {
+		
+		ProxiedPlayer p = e.getPlayer();
+		
+		if(p.hasPermission("support.view")) {
+			if(!EnSupportCommand.getSupportMap().containsKey(p)) {
+				EnSupportCommand.getSupportMap().put(p, new SupportData(true));
+			}
+		}
+		
+	}
+	
+	@EventHandler
+	public void OnPlayerQuit(PlayerDisconnectEvent e) {
+		
+		ProxiedPlayer p = e.getPlayer();
+		
+		if(EnSupportCommand.getSupportMap().containsKey(p)) {
+			EnSupportCommand.getSupportMap().put(p, new SupportData(true));
+		}
+	}
+	
 }
