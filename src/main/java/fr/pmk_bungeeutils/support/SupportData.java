9f19c603458b5039c4db58b63aa5544816df5ce6
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-04-28 22:26:50 +0200

    Ajout de la class SupportData pour la commande EnSupport

---
+++ src/main/java/fr/pmk_bungeeutils/support/SupportData.java
@@ -0,0 +1,21 @@
+package fr.pmk_bungeeutils.support;
+
+public class SupportData {
+
+	public SupportData(boolean s) {
+		this.state = s;
+	}
+	
+	private boolean state;
+
+	public boolean isState() {
+		return state;
+	}
+
+	public void setState(boolean state) {
+		this.state = state;
+	}
+	
+	
+	
+}
