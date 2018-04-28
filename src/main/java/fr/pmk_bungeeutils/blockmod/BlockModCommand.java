c3e230f4d6aa50c3a77990806d07ac6ffc0c5eaf
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-04-28 22:06:31 +0200

    Fix commandes errors

--- src/main/java/fr/pmk_bungeeutils/blockmod/BlockModCommand.java
+++ src/main/java/fr/pmk_bungeeutils/blockmod/BlockModCommand.java
@@ -1,5 +1,6 @@
 package fr.pmk_bungeeutils.blockmod;
 
+import fr.pmk_bungeeutils.MainBungeeUtils;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
@@ -19,8 +20,78 @@ public class BlockModCommand extends Command {
 			
 			ProxiedPlayer p = (ProxiedPlayer)sender;
 			
-			p.sendMessage("[BLOCKMOD] option de la commande : -s true/false ( activé ou non ) / -r raison de blockage / -p permissions ( permissions d'overpass )");
+			if(!p.hasPermission("blockmod.set")) {
+				p.sendMessage("Pas la perm !");
+				return;
+			}
 			
+			p.sendMessage("[BLOCKMOD] option de la commande : -s true/false ( activé ou non ) / -r raison de blockage / -p permissions ( permissions d'overpass ) / -u pour update les joueurs");
+			
+			//if(!(args.length  <= 2)) {
+				
+				String param = args[0];
+				
+				if(param.equalsIgnoreCase("-s")) {
+					
+					String state = args[1];
+					
+					if(state.equalsIgnoreCase("true")) {
+						
+						BlockModManager.setActive(true);
+						return;
+						
+					}else if (state.equalsIgnoreCase("false")) {
+						
+						BlockModManager.setActive(false);
+						return;
+						
+					}else {
+						p.sendMessage("Mauvaise argument !");
+						return;
+					}
+					
+				}else if (param.equalsIgnoreCase("-p")) {
+					
+					String perm = args[1];
+					System.out.println("p : " + perm);
+					
+					BlockModManager.setOverPerm(perm);
+					return;
+					
+				}else if (param.equalsIgnoreCase("-r")) {
+					
+					String raison = "";
+					
+					for (int i = 1; i < args.length; i++) {
+						raison += args[i] + " ";
+					}
+					
+					System.out.println("r : " + raison);
+					
+					BlockModManager.setRaison(raison);
+					return;
+					
+				}if (param.equalsIgnoreCase("-i")) {
+					
+					p.sendMessage("Perm " + BlockModManager.getOverPerm());
+					p.sendMessage("Raison " + BlockModManager.getRaison());
+					p.sendMessage("State " + BlockModManager.isActive());
+					return;
+					
+				}else if (param.equalsIgnoreCase("-u")) {
+					
+					// update des joueurs
+					MainBungeeUtils.updatePlayer();
+					return;
+					
+				}else {
+					p.sendMessage("Mauvaise argument !");
+					return;
+				}
+				
+			}
+			return;
+			/*
 			String arg = "";
 			
 			for (String string : args) {
@@ -29,8 +100,8 @@ public class BlockModCommand extends Command {
 			
 			System.out.println(arg);
 			
-			
-		}
+			*/
+		//}
 
 	}
 
