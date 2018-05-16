20db15a92b388f0de910c2c2802d11b403c86cc7
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-05-16 21:24:08 +0200

    Ajout class de commande diffusion d'information boutique

---
+++ src/main/java/fr/pmk_bungeeutils/pmkbuy/BuyInfoCommand.java
@@ -0,0 +1,19 @@
+package fr.pmk_bungeeutils.pmkbuy;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+public class BuyInfoCommand extends Command {
+
+	public BuyInfoCommand(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute(CommandSender sender, String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
