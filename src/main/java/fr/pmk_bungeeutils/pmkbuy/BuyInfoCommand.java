9943154f9bac0143a505ea442f43fca8335d4d8a
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-05-16 21:40:21 +0200

    Ajout de la commande pmkbuyinfo pour les achats boutiques

--- src/main/java/fr/pmk_bungeeutils/pmkbuy/BuyInfoCommand.java
+++ src/main/java/fr/pmk_bungeeutils/pmkbuy/BuyInfoCommand.java
@@ -1,18 +1,30 @@
 package fr.pmk_bungeeutils.pmkbuy;
 
+import fr.pmk_bungeeutils.MainBungeeUtils;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class BuyInfoCommand extends Command {
 
-	public BuyInfoCommand(String name) {
-		super(name);
+	public BuyInfoCommand() {
+		super("pmkbuyinfo");
 		// TODO Auto-generated constructor stub
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
+		
+		String username = args[0];
+		String montant = args[1];
+		
+		for(ProxiedPlayer p: MainBungeeUtils.getInstance().getProxy().getPlayers()){
+			
+			System.out.println("§l§2[ §aBoutique §2] §r§dMerci à §r§l§n§c#" + username + "# §r§dpour son soutien au serveur de §r§l§n§c#" + montant + "# §r§d!!!");
+			p.sendMessage("§l§2[ §aBoutique §2] §r§dMerci à §r§l§n§c#" + username + "# §r§dpour son soutien au serveur de §r§l§n§c#" + montant + "# §r§d!!!");
+			
+		}
 
 	}
 
