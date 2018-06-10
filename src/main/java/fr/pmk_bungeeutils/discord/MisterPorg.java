526aed909a270455dc1bd1e95e12a6cc9b683ace
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-10 23:15:59 +0200

    Ajout des methodes d'init du bot & getter/setter jda

--- src/main/java/fr/pmk_bungeeutils/discord/MisterPorg.java
+++ src/main/java/fr/pmk_bungeeutils/discord/MisterPorg.java
@@ -5,10 +5,12 @@ import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.hooks.EventListener;
 
 public class MisterPorg {
 
 	private static JDA jda;
+	private boolean isInit;
 	
 	public void start(String token) {
 		
@@ -19,7 +21,9 @@ public class MisterPorg {
 		try {
 			
 			jda = new JDABuilder(AccountType.BOT).setToken(token).buildBlocking();
-			
+			jda.addEventListener(new DiscordEvent());
+			this.isInit = true;
+			return;
 			
 		} catch (LoginException e) {
 			// TODO Auto-generated catch block
@@ -29,6 +33,22 @@ public class MisterPorg {
 			e.printStackTrace();
 		}
 		
+	}
+
+	public static JDA getJda() {
+		return jda;
+	}
+
+	public static void setJda(JDA jda) {
+		MisterPorg.jda = jda;
+	}
+
+	public boolean isInit() {
+		return isInit;
+	}
+
+	public void setInit(boolean isInit) {
+		this.isInit = isInit;
 	}
 	
 }
