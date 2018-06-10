adea7ec6db5203032ae1d987f458f1efbc0639c1
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-10 23:14:47 +0200

    Fix package

--- src/main/java/fr/pmk_bungeeutils/commands/Aide.java
+++ src/main/java/fr/pmk_bungeeutils/commands/Aide.java
@@ -1,5 +1,6 @@
-package fr.pmk_bungeeutils;
+package fr.pmk_bungeeutils.commands;
 
+import fr.pmk_bungeeutils.MainBungeeUtils;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
