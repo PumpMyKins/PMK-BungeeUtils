d9f91623fa551e9a4a6bed8fd332603c831653ba
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-11 17:27:24 +0200

    Fix methods onLinkAccountCommand

--- src/main/java/fr/pmk_bungeeutils/security/SessionLoggerManager.java
+++ src/main/java/fr/pmk_bungeeutils/security/SessionLoggerManager.java
@@ -1,5 +1,7 @@
 package fr.pmk_bungeeutils.security;
 
+import java.util.Collection;
+
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
@@ -22,6 +24,7 @@ public class SessionLoggerManager {
 			if(c.equals("!open-session")) {
 				
 				onOpenSessionCommand(e);
+				return;
 				
 			}else if(c.equals("!history")) {
 				
@@ -40,28 +43,30 @@ public class SessionLoggerManager {
 				onDisableSessionCommand(e,m.getContentDisplay().split(" "));
 				
 			}else {
+				
+				// envoie message de suppression + aide
+				
+				e.getAuthor().openPrivateChannel().complete()
+				.sendMessage("Message envoyé dans le canal **session-logger** non reconnu comme étant une commande valide, celui-ci a été supprimé.")
+				.complete();
+				
 				// commande invalide
 				
 				m.delete().complete();
 				
-				// envoie message de suppression + aide
-				
-				m.getAuthor().openPrivateChannel().complete()
-				.sendMessage("Message envoyé dans le canal **session-logger** non reconnu comme étant une commande valide, celui-ci a été supprimé.")
-				.queue();
-				
 			}
 			
 		}else {
-			// pas commande alors suppression
-			
-			m.delete().complete();
 			
 			// envoie message de suppression + aide
 			
-			m.getAuthor().openPrivateChannel().complete()
+			e.getAuthor().openPrivateChannel().complete()
 			.sendMessage("Message envoyé dans le canal **session-logger** non reconnu comme étant une commande valide, celui-ci a été supprimé.")
-			.queue();
+			.complete();
+			
+			// pas commande alors suppression
+			
+			m.delete().complete();
 			
 		}
 		
@@ -78,12 +83,11 @@ public class SessionLoggerManager {
 		
 	}
 
+	@SuppressWarnings("deprecation")
 	private void onLinkAccountCommand(MessageReceivedEvent e, String... args) {
 		// TODO Auto-generated method stub
 		
-		String playerName = args[1];
-		
-		if(playerName == null) {
+		if(args.length != 2) {
 			
 			e.getMessage().delete().queue();
 			
@@ -94,28 +98,63 @@ public class SessionLoggerManager {
 			
 		}else {
 			
-			ProxiedPlayer p = ProxyServer.getInstance().getPlayer(playerName);
+			String playerName = args[1];
 			
-			if(p == null | !p.isConnected()) {				
+			Collection<ProxiedPlayer> cPlayer = ProxyServer.getInstance().matchPlayer(playerName);
+			
+			if(cPlayer.isEmpty()) {
 				
 				e.getMessage().delete().queue();
 				// pas valide ou non connecté
 				e.getChannel().sendMessage(new MessageBuilder().append(e.getAuthor())
-						.append(" Paramètre de commande invalide, le speudo spécifié n'est pas valide ou alors réfère à un joueur non connecté !")
+						.append(" Paramètre de commande invalide, aucun joueur trouvé !")
 						.build())
 						.queue();
+				
+			}else if(cPlayer.size() == 1) {
+				
+				ProxiedPlayer p = (ProxiedPlayer) cPlayer.toArray()[0];
+				
+				if(SessionLoggerUtils.getLinkMap().containsKey(p.getUniqueId().toString())) {
+					
+					e.getMessage().delete().queue();
+					// déjà une requète en cours
+					e.getChannel().sendMessage(new MessageBuilder().append(e.getAuthor())
+							.append(" Une reqète de liaison pour ce joueur est déjà en attente !")
+							.build())
+							.queue();
+					
+				}else {
+					
+					// player trouvé
+					e.getMessage().delete().queue();
+					// pas valide ou non connecté
+					e.getChannel().sendMessage(new MessageBuilder().append(e.getAuthor())
+							.append(" Envoie de la demande de confirmation en jeu ! Vous disposez de 30 secondes pour y répondre")
+							.build())
+							.queue();
+					
+					// envoie de la demande en jeu
+					
+					
+					SessionLoggerUtils.inGameLink(p,e.getAuthor());
+					
+				}
 				
 			}else {
 				
+				String playerList = "";
+				
+				for (ProxiedPlayer proxiedPlayer : cPlayer) {
+					playerList += " - " + proxiedPlayer.getName() + " \n ";
+				}
+				
 				e.getMessage().delete().queue();
 				// pas valide ou non connecté
 				e.getChannel().sendMessage(new MessageBuilder().append(e.getAuthor())
-						.append(" Envoie de la demande de confirmation en jeu ! Vous disposez de 30 secondes pour y répondre")
+						.append(" Plusieurs joueurs trouvés : \n " + playerList)
 						.build())
 						.queue();
-				
-				// envoie de la demande en jeu
-				
 				
 			}
 			
