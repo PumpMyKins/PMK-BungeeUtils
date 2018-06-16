27599984c9d9299cddd870da756b628240011240
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-16 15:10:11 +0200

    Ajout récupération UUID args command

--- src/main/java/fr/pmk_bungeeutils/security/commands/LinkCommand.java
+++ src/main/java/fr/pmk_bungeeutils/security/commands/LinkCommand.java
@@ -1,6 +1,11 @@
 package fr.pmk_bungeeutils.security.commands;
 
+import java.util.HashMap;
+
+import fr.pmk_bungeeutils.security.LinkSessionData;
+import fr.pmk_bungeeutils.security.SessionLoggerUtils;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class LinkCommand extends Command {
@@ -10,15 +15,49 @@ public class LinkCommand extends Command {
 		// TODO Auto-generated constructor stub
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void execute(CommandSender sender, String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println(args[0]);
-		System.out.println(args[1]);
+		
+		if(!(sender instanceof ProxiedPlayer))
+			return;
+		
+		ProxiedPlayer p = (ProxiedPlayer) sender;
 		
 		if(args.length == 1) {
 			
+			String uuid = args[0];
+			
+			HashMap<String, LinkSessionData> h = SessionLoggerUtils.getLinkMap();
+			
+			if(h.containsKey(p.getUniqueId().toString())) {
+				
+				LinkSessionData d = h.get(p.getUniqueId().toString());
+				
+				String r = d.getRefuse_UUID().toString();
+				String a = d.getAccept_UUID().toString();
+				
+				if(uuid.equals(r)) {
+					// refus
+					p.sendMessage("§9[PumpMyStaff]§c Requête non valide !");
+					
+				}else if(uuid.equals(a)){
+					// accept
+					p.sendMessage("§9[PumpMyStaff]§c Requête non valide !");
+					
+				}else {
+					// invalide
+					p.sendMessage("§9[PumpMyStaff]§c Requête non valide !");
+					
+				}
+				
+			}else {
+				
+				// pas de demande
+				p.sendMessage("§9[PumpMyStaff]§c Requête non valide !");
+				
+			}
 			
 			
 		}else {
