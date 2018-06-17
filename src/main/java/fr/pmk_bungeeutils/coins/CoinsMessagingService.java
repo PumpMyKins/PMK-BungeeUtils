63819650ea0c55781967dbf9b3dc401d66bed967
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-06-17 18:54:22 +0200

    Ajout class listener MessagingServiceCoins

---
+++ src/main/java/fr/pmk_bungeeutils/coins/CoinsMessagingService.java
@@ -0,0 +1,16 @@
+package fr.pmk_bungeeutils.coins;
+
+import net.md_5.bungee.api.event.PluginMessageEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+public class CoinsMessagingService implements Listener {
+
+	@EventHandler
+	public void OnMessagingServiceReceive(PluginMessageEvent e) throws Exception {
+		
+		
+		
+	}
+	
+}
