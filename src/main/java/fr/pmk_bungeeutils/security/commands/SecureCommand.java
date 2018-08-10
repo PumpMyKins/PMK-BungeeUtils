c818910883ed7d40afc6ce22de3e7c1d87874bfa
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-08-10 12:32:42 +0200

    Ajout de la class secure commande

---
+++ src/main/java/fr/pmk_bungeeutils/security/commands/SecureCommand.java
@@ -0,0 +1,23 @@
+package fr.pmk_bungeeutils.security.commands;
+
+import fr.pmk_bungeeutils.MainBungeeUtils;
+import fr.pmk_bungeeutils.blockmod.BlockModManager;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class SecureCommand extends Command {
+
+	public SecureCommand(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute(CommandSender sender, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
