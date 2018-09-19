d2e6f58ba740571c3d8860788edd1ec707f0caea
evroxfr <evroxfr@gmail.com>
2018-09-19 12:21:43 +0200

    /bcall <message> (bcall.use)

--- src/main/java/fr/pmk_bungeeutils/commands/BroadcastAll.java
+++ src/main/java/fr/pmk_bungeeutils/commands/BroadcastAll.java
@@ -1,6 +1,9 @@
 package fr.pmk_bungeeutils.commands;
 
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class BroadcastAll extends Command {
@@ -11,7 +14,26 @@ public class BroadcastAll extends Command {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
+		
+		if(sender instanceof ProxiedPlayer) {
+			ProxiedPlayer player = (ProxiedPlayer) sender;
+			
+			if(player.hasPermission("bcall.use")) {
+				if(args.length == 0) {
+					player.sendMessage(new TextComponent("§cEssayez /bcall <message>"));
+				}
+				if(args.length >= 1) {
+					StringBuilder bc = new StringBuilder();
+					for(String port : args) {
+						bc.append(port + " ");
+					}
+					
+					ProxyServer.getInstance().broadcast(new TextComponent("§9[Broadcast] " + bc.toString()));
+				}
+			}else {
+				player.sendMessage(new TextComponent("§cVous n'avez pas la permission !"));
+			}
+		}
 
 	}
 
