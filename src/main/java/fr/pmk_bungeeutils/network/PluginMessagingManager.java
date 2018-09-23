ac493bcc38584bad68343401023b84912428c085
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-09-23 12:49:18 +0200

    Ajout class et methodes utiles pour messaging API

--- src/main/java/fr/pmk_bungeeutils/network/PluginMessagingManager.java
+++ src/main/java/fr/pmk_bungeeutils/network/PluginMessagingManager.java
@@ -1,7 +1,37 @@
 package fr.pmk_bungeeutils.network;
 
-public class PluginMessagingManager {
+import fr.pmk_bungeeutils.MainBungeeUtils;
+import net.md_5.bungee.api.plugin.Listener;
 
+public class PluginMessagingManager implements Listener {
+
+	private static MainBungeeUtils main;
+	private static PluginMessagingManager plgM;
+
+	public static PluginMessagingManager init(MainBungeeUtils m) {
+		
+		plgM = new PluginMessagingManager(m);		
+		
+		startListen();
+		
+		return plgM;		
+	}
 	
+	public static void startListen() {
+		
+		main.getProxy().getPluginManager().registerListener(main, plgM);	// register event
+		
+	}
+	
+	public static void stopListen() {
+		
+		main.getProxy().getPluginManager().unregisterListener(plgM); 	// unregister event
+		
+	}
+	
+	public PluginMessagingManager(MainBungeeUtils m) {
+		// TODO Auto-generated constructor stub
+		
+	}
 	
 }
