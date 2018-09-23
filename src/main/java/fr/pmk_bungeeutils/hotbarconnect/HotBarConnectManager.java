358dfef8980466bafc95ea6156de24c302e8ccf9
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-09-23 18:11:19 +0200

    Création class hbc manager & ajout hashMap servers

---
+++ src/main/java/fr/pmk_bungeeutils/hotbarconnect/HotBarConnectManager.java
@@ -0,0 +1,12 @@
+package fr.pmk_bungeeutils.hotbarconnect;
+
+import java.util.HashMap;
+
+import fr.pmk_bungeeutils.hotbarconnect.data.HbcServer;
+
+public class HotBarConnectManager {
+
+	private static HashMap<String, HbcServer> hashServer = new HashMap<>();
+	
+	
+}
