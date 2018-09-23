30039a060c1310a1f886463e17bf62b4c7c71c69
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-09-23 18:13:25 +0200

    Ajout getter/setter hashServer

--- src/main/java/fr/pmk_bungeeutils/hotbarconnect/HotBarConnectManager.java
+++ src/main/java/fr/pmk_bungeeutils/hotbarconnect/HotBarConnectManager.java
@@ -7,6 +7,14 @@ import fr.pmk_bungeeutils.hotbarconnect.data.HbcServer;
 public class HotBarConnectManager {
 
 	private static HashMap<String, HbcServer> hashServer = new HashMap<>();
+
+	public static HashMap<String, HbcServer> getServers() {
+		return hashServer;
+	}
+
+	public static void setHashServers(HashMap<String, HbcServer> hashServer) {
+		HotBarConnectManager.hashServer = hashServer;
+	}
 	
 	
 }
