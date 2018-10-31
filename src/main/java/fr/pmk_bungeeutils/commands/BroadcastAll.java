296b52f97c8ffc07fb59497eb199e1252e3aadc3
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-10-31 17:48:40 +0100

    Fix bc commande

--- src/main/java/fr/pmk_bungeeutils/commands/BroadcastAll.java
+++ src/main/java/fr/pmk_bungeeutils/commands/BroadcastAll.java
@@ -33,6 +33,20 @@ public class BroadcastAll extends Command {
 			}else {
 				player.sendMessage(new TextComponent("§cVous n'avez pas la permission !"));
 			}
+		}else {
+			
+			if(args.length == 0) {
+				sender.sendMessage(new TextComponent("§cEssayez /bcall <message>"));
+			}
+			if(args.length >= 1) {
+				StringBuilder bc = new StringBuilder();
+				for(String port : args) {
+					bc.append(port + " ");
+				}
+				
+				ProxyServer.getInstance().broadcast(new TextComponent("§6§l[§r§2Pump§eMy§aKins§r§6§l]§r " + bc.toString()));
+			}
+			
 		}
 
 	}
